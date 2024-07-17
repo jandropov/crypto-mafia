2409b48c001b76e26bdaa28863a08f29b44d1bb1
iam <smb@mail.ru>
2024-07-17 12:12:29 +0300

    lql2

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,5 +7,5 @@ workers = 4  # Adjust based on your server's resources
 worker_connections = 1000
 threads = 4
 
-certfile = "/etc/letsencrypt/live/app.crypto-mafia.xyz/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/app.crypto-mafia.xyz/privkey.pem"
+# certfile = "/etc/letsencrypt/live/app.crypto-mafia.xyz/fullchain.pem"
+# keyfile = "/etc/letsencrypt/live/app.crypto-mafia.xyz/privkey.pem"
